a6d04e2663eaeb243600ccd908e5698d7ebec539
John McLear <john@mclear.co.uk>
2020-11-22 16:24:43 +0000

    autofixes from Etherpad checkPlugins.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var eejs = require('ep_etherpad-lite/node/eejs')
-  , express = require('ep_etherpad-lite/node_modules/express')
+const eejs = require('ep_etherpad-lite/node/eejs');
+const express = require('ep_etherpad-lite/node_modules/express');
 
-exports.expressServer = function(hook, args, cb) {
-  args.app.get('/metrics', function(req, res) {
-    res.send(eejs.require("ep_ether-o-meter/templates/metrics.html"))
-  })
-  
-  args.app.use('/metrics/static', express.static(__dirname + '/static'))
-}
+exports.expressServer = function (hook, args, cb) {
+  args.app.get('/metrics', (req, res) => {
+    res.send(eejs.require('ep_ether-o-meter/templates/metrics.html'));
+  });
+
+  args.app.use('/metrics/static', express.static(`${__dirname}/static`));
+};
